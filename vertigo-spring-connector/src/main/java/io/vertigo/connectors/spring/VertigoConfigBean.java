fabb1d23da72f13ba20d31963a825318a918db3e
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-07-28 17:34:41 +0200

    [all] fix rename

--- vertigo-spring-connector/src/main/java/io/vertigo/connectors/spring/VertigoConfigBean.java
+++ vertigo-spring-connector/src/main/java/io/vertigo/connectors/spring/VertigoConfigBean.java
@@ -4,16 +4,16 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
-import io.vertigo.core.node.Home;
+import io.vertigo.core.node.App;
 import io.vertigo.core.node.component.CoreComponent;
 
 public class VertigoConfigBean implements BeanFactoryPostProcessor {
 
 	@Override
 	public void postProcessBeanFactory(final ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		Home.getApp().getComponentSpace().keySet()
+		App.getApp().getComponentSpace().keySet()
 				.stream()
-				.forEach(key -> beanFactory.registerSingleton(key, Home.getApp().getComponentSpace().resolve(key, CoreComponent.class)));
+				.forEach(key -> beanFactory.registerSingleton(key, App.getApp().getComponentSpace().resolve(key, CoreComponent.class)));
 
 	}
 
