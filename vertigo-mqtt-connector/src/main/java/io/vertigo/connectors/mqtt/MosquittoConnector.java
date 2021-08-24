60d71ecf4fc4695dc0121b7840d9dd1183fb9f38
phlppchrtn <philippe.chretien@kleegroup.com>
2021-08-24 15:51:11 +0200

    fix record

--- vertigo-mqtt-connector/src/main/java/io/vertigo/connectors/mqtt/MosquittoConnector.java
+++ vertigo-mqtt-connector/src/main/java/io/vertigo/connectors/mqtt/MosquittoConnector.java
@@ -49,7 +49,7 @@ public class MosquittoConnector implements Connector<MqttClient>, Activeable {
 		Assertion.check().isNotBlank(brokerHost);
 		//---
 		connectionName = connectionNameOpt.orElse("main");
-		final String clientId = clientIdOpt.orElseGet(() -> Node.getNode().getNodeConfig().getNodeId());
+		final String clientId = clientIdOpt.orElseGet(() -> Node.getNode().getNodeConfig().nodeId());
 		try {
 			mqttClient = new MqttClient(brokerHost, clientId + connectionName, new MemoryPersistence());
 			final MqttConnectOptions connOpts = new MqttConnectOptions();
