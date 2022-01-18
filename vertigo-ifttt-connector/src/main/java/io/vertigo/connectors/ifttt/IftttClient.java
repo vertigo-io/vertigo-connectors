1bd9bff95d8004a16b516d395a043a50914ae42f
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2022-01-18 15:14:54 +0100

    [all] update dependencies
    msal4j -> 1.11.0
    influxdb-client-java -> 4.0.0
    javalin -> 4.3.0
    keycloak-servlet-filter-adapter -> 16.1.0
    mongodb-driver-sync -> 4.4.1
    redis -> 4.0.1
    spring -> 5.3.15
    jersey-client -> 3.0.3

--- vertigo-ifttt-connector/src/main/java/io/vertigo/connectors/ifttt/IftttClient.java
+++ vertigo-ifttt-connector/src/main/java/io/vertigo/connectors/ifttt/IftttClient.java
@@ -19,19 +19,18 @@ package io.vertigo.connectors.ifttt;
 
 import java.util.Optional;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation.Builder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status.Family;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.lang.VSystemException;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation.Builder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status.Family;
 
 /**
  *
