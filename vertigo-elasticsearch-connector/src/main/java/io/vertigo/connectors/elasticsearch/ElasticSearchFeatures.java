c933436e22b68c008227d3e21675d68a3ea1b42e
NPi2Loup <npi2loup@gmail.com>
2026-03-09 17:55:49 +0100

    [elastic] add comment for embeddedServer

--- vertigo-elasticsearch-connector/src/main/java/io/vertigo/connectors/elasticsearch/ElasticSearchFeatures.java
+++ vertigo-elasticsearch-connector/src/main/java/io/vertigo/connectors/elasticsearch/ElasticSearchFeatures.java
@@ -41,9 +41,14 @@ public final class ElasticSearchFeatures extends Features<ElasticSearchFeatures>
 				.addConnector(RestElasticSearchConnector.class, params);
 		return this;
 	}
-	
+
 	@Feature("embeddedServer")
 	public ElasticSearchFeatures withEmbeddedServer(final Param... params) {
+		/**
+		 * To use EmbeddedServer with org.testcontainers, we need to declare a varEnv DOCKER_HOST to point on docker socket
+		 * To active socket on a docket with WSL, you shoud edit "/lib/systemd/system/docker.service" and add this line in [Service] section:
+		 * ExecStart=/usr/bin/dockerd -H fd:// -H tcp://127.0.0.1:2375 --containerd=/run/containerd/containerd.sock
+		 */
 		getModuleConfigBuilder()
 				.addComponent(EmbeddedElasticSearchServer.class, params);
 		return this;
